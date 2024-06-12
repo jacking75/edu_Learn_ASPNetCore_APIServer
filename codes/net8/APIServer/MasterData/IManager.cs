294be5c38d11a1a8c6093bdb636c6898aafc83ee
heungbae <heungabe@com2us.com>
2024-06-12 16:07:47 +0900

    삭제. 정리 중

--- codes/net8/APIServer/MasterData/IManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Threading.Tasks;
-
-namespace APIServer.MasterData;
-
-public interface IManager : IDisposable
-{
-    public Task<bool> Load(string connectionString);
-}
