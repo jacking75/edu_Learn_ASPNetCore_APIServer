aea0eeb43ec23ef38839e51d43b4d30ed729a4c3
heungbae <heungabe@com2us.com>
2024-06-12 16:53:21 +0900

    코드 정리 중

---
+++ codes/APIServer_Templete/MatchAPIServer/Controllers/CheckMatcingController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ZLogger;
+using static APIServer.Controllers.CheckMatching;
+
+namespace APIServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CheckMatching : Controller
+{
+    IMatchWoker _matchWorker;
+
+
+    public CheckMatching(IMatchWoker matchWorker)
+    {
+        _matchWorker = matchWorker;
+    }
+
+    [HttpPost]
+    public CheckMatchingRes Post(CheckMatchingReq request)
+    {
+        CheckMatchingRes response = new();
+
+        (var result, var completeMatchingData) = _matchWorker.GetCompleteMatching(request.UserID);
+        
+        //TODO: 결과를 담아서 보낸다
+
+        return response;
+    }
+
+
+}
+
+public class CheckMatchingReq
+{
+    public string UserID { get; set; }
+}
+
+
+public class CheckMatchingRes
+{
+    public ErrorCode Result { get; set; } = ErrorCode.None;
+    public string ServerAddress { get; set; } = "";
+    public int RoomNumber { get; set; } = 0;    
+}
